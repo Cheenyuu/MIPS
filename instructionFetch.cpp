897e07dc7fbe381105226cb662777aaf25dbe359
Chenyu Zheng <chnyzheng@gmail.com>
2025-07-27 11:59:12 -0400

    first push, adding initial code

---
+++ instructionFetch.cpp
@@ -0,0 +1,1 @@
+//There are four main components: Instruction
---
+++ mips.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+void run(){
+    bool running = false;
+    while(running == true){
+        cout<<"Code ran incorrectly";
+    }
+    cout<<"Code ran correctly";
+}
+
+
+int main(){
+    
+    run();
+
+}
