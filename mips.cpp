0d772618f7354510691e902d7d81326d7a546a5a
Chenyu Zheng <chnyzheng@gmail.com>
2025-07-29 12:51:17 -0400

    testing PC_adder, making sure it just works

--- mips.cpp
+++ mips.cpp
@@ -10,8 +10,21 @@ void run(){
     uint32_t mem(uint32_t instructionMem[], uint32_t currentInst);
     void IFID(uint32_t* addr, uint32_t* inst);*/
 
-    uint32_t instruction = mem(ARR, 0);
-    cout<<instruction;
+    uint32_t instruction = NULL;
+    uint32_t currentAddress = 0x0;
+
+    cout<<currentAddress<<endl;
+    PC_adder(&currentAddress);
+    cout<<currentAddress<<endl;
+
+    /*bool running = true;
+    while(running){
+        instruction = mem(ARR, currentAddress);
+        IFID(&currentAddress, &instruction);
+
+        PC_adder(&currentAddress);
+    }*/
+
 }
 
 
